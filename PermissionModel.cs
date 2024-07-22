efed8e2dfc688c1e39bb0a202e8b7acf6ae3684e
Saurabh Adhikari <sauravadhikari98@gmail.com>
2024-07-22 15:25:20 +0545

    design permission model

---
+++ PermissionModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PermissionMigrator
+{
+    public class Resource
+    {
+        public Role Role { get; set; }
+    }
+    public class Role
+    {
+        public List<string> Client { get; set; }
+        public List<string> SalesTeam { get; set; }
+        public List<string> Employee { get; set; }
+        public List<string> Manager { get; set; }
+        public List<string> Director { get; set; }
+        public List<string> Admin { get; set; }
+        public List<string> Prospect { get; set; }  
+    }
+}
